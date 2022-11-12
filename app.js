c8841a74b3e0620e13fffe488e7a84f3618d4fa6
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-11-12 19:01:59 -0300

    chore: setup view engine ejs

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('./cors');
 const routers = require('./src/config/routers');
 
 const app = express();
 
 app.options('*', cors);
-app.use(cors());
+
+app.use(express.static('public'));
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(helmet);
+app.use(helmet());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
+
 routers(app);
 
 module.exports = app;
