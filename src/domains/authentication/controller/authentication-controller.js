beed519bdff79ebc660061963599ebbad9f5e448
Marcos Victor Da Silva Barbosa <marcosvictorsb@gmail.com>
2022-12-24 17:01:29 -0300

    chore: return status code inside json

--- src/domains/authentication/controller/authentication-controller.js
+++ src/domains/authentication/controller/authentication-controller.js
@@ -14,8 +14,8 @@ class AuthenticationController extends IController {
   async authenticate(request, response) {
     try {
       const { email, password } = await this.validator.validateBodyParams(request.body);
-      const result = await this.serviceAuth.authenticate(email, password);
-      return response.status(result.status).json(result.body);
+      const { status, body } = await this.serviceAuth.authenticate(email, password);
+      return response.status(status).json({ status, body });
     } catch (error) {
       return this.errorHandler(error, request, response);
     }
